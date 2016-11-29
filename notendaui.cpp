d1d651706e04d6c59e82f81adc7ab1f37bea26d3
agustha16 <agustha16@ru.is>
2016-11-29 10:22:42 +0000

    Checkan

--- notendaui.cpp
+++ notendaui.cpp
@@ -9,43 +9,57 @@ NotendaUI::NotendaUI()
 
 }
 
+bool check = true;
+
 void NotendaUI::keyra()
 {
-    cout << "*==============================================================*" << endl;
-    cout << "*||Please enter one the following commands*                  ||*" << endl;
-    cout << "*==============================================================*" << endl;
-    cout << "*||list - Shows a list of all known entries in the database. ||*" << endl;
-    cout << "*||add - Add a new entry into the database.                  ||*" << endl;
-    cout << "*||delete - Removes an entry from the database.              ||*" << endl;
-    cout << "*||update - Updates an entry from the database.              ||*" << endl;
-    cout << "*||purge - Removes every entry from the database.            ||*" << endl;
-    cout << "*==============================================================*" << endl;
 
-    string skipun;
-    cin >> skipun;
+    do
+        {
+        cout << "*==============================================================*" << endl;
+        cout << "*||Please enter one the following commands*                  ||*" << endl;
+        cout << "*==============================================================*" << endl;
+        cout << "*||list - Shows a list of all known entries in the database. ||*" << endl;
+        cout << "*||add - Add a new entry into the database.                  ||*" << endl;
+        cout << "*||delete - Removes an entry from the database.              ||*" << endl;
+        cout << "*||update - Updates an entry from the database.              ||*" << endl;
+        cout << "*||purge - Removes every entry from the database.            ||*" << endl;
+        cout << "*==============================================================*" << endl;
 
-    if (skipun == "list" || skipun == "l")
-    {
+        string skipun;
+        cin >> skipun;
+        system("cls");
 
-    }
+        if (skipun == "list" || skipun == "l")
+        {
 
-    else if (skipun == "add" || skipun == "a")
-    {
+        }
 
-    }
+        else if (skipun == "add" || skipun == "a")
+        {
 
-    else if (skipun == "delete" || skipun == "d")
-    {
+        }
 
-    }
+        else if (skipun == "delete" || skipun == "d")
+        {
 
-    else if (skipun == "update" || skipun == "u")
-    {
+        }
 
-    }
+        else if (skipun == "update" || skipun == "u")
+        {
 
-    else if (skipun == "purge" || skipun == "p")
-    {
+        }
 
-    }
+        else if (skipun == "purge" || skipun == "p")
+        {
+
+        }
+
+        else
+        {
+            cerr << "Input not valid, try again." << endl;
+            cin >> skipun;
+            check = false;
+        }
+    } while (check == false);
 }
