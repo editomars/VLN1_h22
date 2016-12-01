c172e5cb57d904ad69653d9466b2eff9e31bbc98
Gunnar Baldursson <gunnarb16@ru.is>
2016-12-01 19:17:22 +0000

    Fjarlægði redundant prentaRadad fall þar sem það gerði sama hlut og prentalista

--- notendaui.cpp
+++ notendaui.cpp
@@ -279,7 +279,15 @@ void NotendaUI::leitaGrein() //Search / Filter UI grein
             getline(cin,nafn);
             cout << endl;
             gogn = _service.leitaStreng("nafn", nafn);
-            prentaRadad(gogn);
+            if (gogn.size() == 0)
+            {
+                hausUI();
+                cout << "|No person with those parameters exists in the database.                                                 |" << endl;
+                cout << "----------------------------------------------------------------------------------------------------------" << endl;
+            }else
+            {
+                prentaLista(gogn);
+            }
         }
 
         else if (skipunin == "age" || skipunin == "a")
@@ -290,7 +298,15 @@ void NotendaUI::leitaGrein() //Search / Filter UI grein
             cin >> age;
             cout << endl;
             gogn = _service.leitaHeiltolu("aldur", age);
-            prentaRadad(gogn);
+            if (gogn.size() == 0)
+            {
+                hausUI();
+                cout << "|No person with those parameters exists in the database.                                                 |" << endl;
+                cout << "----------------------------------------------------------------------------------------------------------" << endl;
+            }else
+            {
+                prentaLista(gogn);
+            }
         }
 
         else if (skipunin == "birth" || skipunin == "b")
@@ -301,7 +317,15 @@ void NotendaUI::leitaGrein() //Search / Filter UI grein
             cin >> birth;
             cout << endl;
             gogn = _service.leitaHeiltolu("faedingarar", birth);
-            prentaRadad(gogn);
+            if (gogn.size() == 0)
+            {
+                hausUI();
+                cout << "|No person with those parameters exists in the database.                                                 |" << endl;
+                cout << "----------------------------------------------------------------------------------------------------------" << endl;
+            }else
+            {
+                prentaLista(gogn);
+            }
         }
 
         else if (skipunin == "death" || skipunin == "d")
@@ -312,7 +336,15 @@ void NotendaUI::leitaGrein() //Search / Filter UI grein
             cin >> death;
             cout << endl;
             gogn = _service.leitaHeiltolu("danarar", death);
-            prentaRadad(gogn);
+            if (gogn.size() == 0)
+            {
+                hausUI();
+                cout << "|No person with those parameters exists in the database.                                                 |" << endl;
+                cout << "----------------------------------------------------------------------------------------------------------" << endl;
+            }else
+            {
+                prentaLista(gogn);
+            }
         }
 
         else if (skipunin == "return" || skipunin == "r")
@@ -709,20 +741,4 @@ void NotendaUI::hausUI()
     cout << "|Scientist ID \t |Name \t\t\t\t |Gender \t |Year of Birth  |Year of death  |Age \t |" << endl;
     cout << "----------------------------------------------------------------------------------------------------------" << endl;
 }
-
-void NotendaUI::prentaRadad(const vector<tolvufolk>& gogn)
-{
-    hausUI();
-    for(size_t i = 0; i < gogn.size(); i++)
-    {
-            cout << "|" << i + 1 << " \t\t ";
-            cout << gogn[i];
-    }
-    if (gogn.size() == 0)
-    {
-        cout << "|No person with those parameters exists in the database.                                                 |" << endl;
-    }
-    cout << "----------------------------------------------------------------------------------------------------------" << endl;
-}
-
 //------------------------------- Svæðu fyrir cout blokkir endar -------------------------------
