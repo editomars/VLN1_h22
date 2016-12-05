d371062205401f90ad4175019a01db849137eb32
Gunnar Baldursson <gunnarb16@ru.is>
2016-12-05 14:27:46 +0000

    bætti við 2 föllum í header

--- Vika2/sqltenging.h
+++ Vika2/sqltenging.h
@@ -3,15 +3,20 @@
 
 #include <QtSql>
 #include <vector>
+#include <string>
 #include "tolvufolk.h"
 
 class sqltenging
 {
 public:
     sqltenging();
+
+    void baetaVidTolvuFolk(string fNafn, string mNafn, string eNafn, char kyn, int fAr, int dAr);
+    void baetaVidTolvuVel(string nafn, int bAr, bool byggd, string tegund);
+
     vector<tolvufolk> lesa();
 private:
-    QSqlDatabase db;
+    QSqlDatabase _db;
 };
 
 #endif // SQLTENGING_H
