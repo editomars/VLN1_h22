aaa25e239ac1dd9c89d6fe6e13bf616f55fe6f00
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2016-12-15 17:15:29 +0000

    örva takkar flakka á milli tabs

--- vika3/utils/constants.h
+++ vika3/utils/constants.h
@@ -17,10 +17,8 @@ namespace constants
     const int R_KEY_NUMBER = 82;
     const int F5_KEY_NUMBER = 16777268;
     const int CONTROL_KEY_NUMBER = 16777249;
-
-
-
-
+    const int LEFT_ARROW = 16777234;
+    const int RIGHT_ARROW = 16777236;
 }
 
 #endif // CONSTANTS_H
